70eb5f0cb07f6d2a016eb80720e92c413c941b62
Lincoln W Daniel <lwdthe1@gmail.com>
2023-11-10 16:00:20 -0500

    Ignore eslint no-prototype-builtins for deepFreeze util

--- src/internal/utils.ts
+++ src/internal/utils.ts
@@ -3,6 +3,7 @@ export const deepFreeze = (o) => {
 
   Object.getOwnPropertyNames(o).forEach(function (prop) {
     if (
+      // eslint-disable-next-line no-prototype-builtins
       o.hasOwnProperty(prop) &&
       o[prop] !== null &&
       (typeof o[prop] === 'object' || typeof o[prop] === 'function') &&
